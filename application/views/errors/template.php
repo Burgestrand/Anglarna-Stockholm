82748a88325a11ff805a0921f5d7f8f9e8e0c619
Kim Burgestrand <kim@burgestrand.se>
2009-12-21 05:28:00 +0100

    Adds error handling and smoother production configuration

---
+++ application/views/errors/template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+    "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+    <head>
+        <title><?php echo $title ?></title>
+        
+        <link rel="stylesheet" href="/css/error.css" type="text/css" media="screen" charset="utf-8">
+    </head>
+    <body>
+        <?php echo $content ?> 
+    </body>
+</html>
