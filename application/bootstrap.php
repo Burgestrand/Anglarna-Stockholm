Burgestrand/Anglarna-Stockholm

commit 46e48f9bccb852e48f25de59defde4fbe72616f4

Removes unnecessary code and changes to HTML5 doctype

application/bootstrap.php
@@ -123,12 +123,6 @@
                            ->set('content', View::factory('errors/404'));
     }
     
-    if ($request->send_headers()->response)
-    {
-        // Post-modify response
-        // $request->response = str_replace(array(), array(), $request->response);
-    }
-    
     /**
      * Display request response
      */

application/views/errors/template.php
@@ -1,7 +1,7 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-    "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html>
 <html>
     <head>
+        <meta http-equiv="Content-type" content="text/html;charset=utf8">
         <title><?php echo $title ?></title>
         
         <link rel="stylesheet" href="/css/error.css" type="text/css" media="screen" charset="utf-8">

commit af7446d79e94f07fa02d0448a8fdc2044a1b0935

Fixes bugs introduced by 46e48f9bccb852e48f25de59defde4fbe72616f4

application/bootstrap.php
@@ -102,7 +102,7 @@
      * Execute the main request. A source of the URI can be passed, eg: $_SERVER['PATH_INFO'].
      * If no source is specified, the URI will be automatically detected.
      */
-    $request = Request::instance();
+    $request = Request::instance($_SERVER['REQUEST_URI']);
     
     try
     {
@@ -126,4 +126,4 @@
     /**
      * Display request response
      */
-    echo $request->response;
+    echo $request->send_headers()->response;

application/views/errors/template.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-type" content="text/html;charset=utf8">
-        <title><?php echo $title ?></title>
+        <title><?php echo __("error/{$status}") ?></title>
         
         <link rel="stylesheet" href="/css/error.css" type="text/css" media="screen" charset="utf-8">
     </head>