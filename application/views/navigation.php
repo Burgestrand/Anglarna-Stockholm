60aea5bee94d0e300d82feab18c9637057b0e3e9
Kim Burgestrand <kim@burgestrand.se>
2010-01-07 17:31:45 +0100

    Adds adding “active” class to navigation

--- application/views/navigation.php
+++ application/views/navigation.php
@@ -1,5 +1,11 @@
 <ul class="nav">
-    <?php foreach(array('search') as $link): ?> 
-    <li><?php echo html::anchor($link, UTF8::ucfirst($link)) ?></li>
-    <?php endforeach; ?> 
+<?php
+    foreach(array('index') as $link)
+    {
+        printf('<li%s>%s</li>',
+                html::attributes(array('class' => Request::instance()->controller
+                                                  == $link ? 'active' : NULL)),
+                html::anchor($link, UTF8::ucfirst($link)));
+    }
+?> 
 </ul>
