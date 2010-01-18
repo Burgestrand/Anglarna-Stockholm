17b3b033786e6a06397e33f98ea03b1239076eb6
Kim Burgestrand <kim@burgestrand.se>
2010-01-18 11:23:41 +0100

    Auth now uses Sprig instead of ORM

--- application/config/auth.php
+++ application/config/auth.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
     return array(
-        'driver' => 'ORM',
+        'driver' => 'Sprig',
         'hash_method' => 'sha256',
         'salt_pattern' => '1, 3, 5, 7, 11, 13, 17, 19, 23, 31, 33',
         'lifetime' => 60 * 60 * 24 * 14, // remembered for two weeks
