e665dc7a0f38913b59204630cceb46af8af42b6f
Kim Burgestrand <kim@burgestrand.se>
2010-02-06 18:29:54 +0100

    Adds “Request::redirect_back” (to referrer)

---
+++ application/classes/request.php
@@ -0,0 +1,35 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+    /**
+     * An extension of the Request object
+     *
+     * @package     Änglarna STHLM
+     * @author      Kim Burgestrand
+     * @license     GNU GPLv3 http://www.gnu.org/licenses/gpl-3.0.txt
+     */
+    class Request extends Kohana_Request
+    {
+        /**
+         * Redirects the user back to where they came from
+         * 
+         * If the referring page cannot be found it falls back to the
+         * default, specified in the first parameter.
+         * 
+         * @param default
+         * @param code HTTP status code (default: 303)
+         * @return void
+         */
+        public function redirect_back($default = '/', $code = 303)
+        {
+            if (empty(Request::$referrer))
+            {
+                $this->redirect($default, $code);
+            }
+            else
+            {
+                $this->redirect(Request::$referrer, $code);
+            }
+        }
+    }
+
+/* End of file request.php */
+/* Location: ./application/classes/request.php */ 
