bccda742bace5197794ce80af7a6a11c666a4238
Kim Burgestrand <kim@burgestrand.se>
2010-02-16 22:09:38 +0100

    Login now lasts until browser close or logout

--- application/config/session.php
+++ application/config/session.php
@@ -1,12 +1,10 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-return array('native' => array(
-                 'name'      => 'phpsession', // ??
-                 'lifetime'  => 60 * 90, // session valid 90 minutes
-              ),
-             'database' => array(
+return array('database' => array(
                  'group' => 'default', // database connection to use
                  'table' => 'sessions', // table to store session data in
+                 'name' => 'angelsession',
+                 'lifetime' => 0, // session exits only when browser does
              ));
 
 /* End of file session.php */
