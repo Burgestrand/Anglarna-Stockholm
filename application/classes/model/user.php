Burgestrand/Anglarna-Stockholm

commit 807cd96ee7951eb0f61d2e2f54c5be378b7012e4

Update Sprig-Auth to use a smaller database schema

application/classes/model/user.php
@@ -19,16 +19,11 @@
             );
             
             // Fix fields
-            $username = $this->_fields['username'];
-            $username->min_length = 2;
-            $username->max_length = 50;
-            $username->rules = array();
+            $this->_fields['username'] = new Sprig_Field_Username;
             
             $email = $this->_fields['email'];
             $email->column = 'e-mail';
             $email->max_length = 255;
-            
-            unset($this->_fields['password_confirm']);
         }
         
         /**

application/classes/sprig/field/username.php
@@ -10,6 +10,8 @@
     {
         public $min_length = 2;
         public $max_length = 50;
+        public $empty = FALSE;
+        public $unique = TRUE;
     }
     
 /* End of file username.php */

application/sql/database.sql
@@ -16,31 +16,6 @@ INSERT INTO `forums` (`id`, `name`, `description`) VALUES
 ('2', 'Stängt Forum', 'Ett stängt forum där bara Änglar kan läsa och skriva'),
 ('3', 'Nyheter', 'Enbart utvalda Änglar kan läsa och skriva här. Varje inlägg hamnar på framsidan.');
 
-CREATE TABLE `forums_roles` (
-   `forum_id` int(11) unsigned not null,
-   `role_id` int(11) unsigned not null,
-   PRIMARY KEY (`forum_id`,`role_id`),
-   KEY `role_id` (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `forums_roles` (`forum_id`, `role_id`) VALUES 
-('2', '3'),
-('3', '4');
-
-CREATE TABLE `invites` (
-   `token` char(40) not null,
-   `e-mail` varchar(255) not null,
-   `inviter` int(11) unsigned not null,
-   `invitee` int(11) unsigned,
-   PRIMARY KEY (`token`),
-   UNIQUE KEY (`e-mail`),
-   KEY `inviter` (`inviter`),
-   KEY `invitee` (`invitee`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `invites` (`token`, `e-mail`, `inviter`, `invitee`) VALUES 
-('1ba8a4f1778c52e8d6e766acfce17cc87203ebd8', 'kim@burgestrand.se', '1', NULL);
-
 CREATE TABLE `posts` (
    `id` int(11) unsigned not null auto_increment,
    `forum_id` int(11) unsigned not null,
@@ -53,6 +28,7 @@ CREATE TABLE `posts` (
    KEY `forum_id` (`forum_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- | Available roles
 CREATE TABLE `roles` (
    `id` int(11) unsigned not null auto_increment,
    `name` varchar(32) not null,
@@ -68,12 +44,25 @@ INSERT INTO `roles` (`id`, `name`, `description`) VALUES
 ('4', 'reporter', 'Is allowed to read and post in the news forum.'),
 ('5', 'moderator', 'Has the ability to alter forum posts (eg. modify, delete).');
 
+-- | Roles assigned to users
 CREATE TABLE `roles_users` (
    `user_id` int(10) unsigned not null,
    `role_id` int(10) unsigned not null,
    PRIMARY KEY (`user_id`,`role_id`),
    KEY `fk_role_id` (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- | Required roles for each forum
+CREATE TABLE `forums_roles` (
+   `forum_id` int(11) unsigned not null,
+   `role_id` int(11) unsigned not null,
+   PRIMARY KEY (`forum_id`,`role_id`),
+   KEY `role_id` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `forums_roles` (`forum_id`, `role_id`) VALUES 
+('2', '3'),
+('3', '4');
 
 CREATE TABLE `sessions` (
    `session_id` varchar(24) not null,
@@ -83,38 +72,38 @@ CREATE TABLE `sessions` (
    KEY `last_active` (`last_active`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `user_tokens` (
-   `id` int(11) unsigned not null auto_increment,
-   `user_id` int(11) unsigned not null,
-   `user_agent` varchar(40) not null,
-   `token` varchar(32) not null,
-   `created` int(10) unsigned not null,
-   `expires` int(10) unsigned not null,
-   PRIMARY KEY (`id`),
-   UNIQUE KEY (`token`),
-   KEY `fk_user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- [Table `user_tokens` is empty]
-
+-- | Registered users
 CREATE TABLE `users` (
    `id` int(11) unsigned not null auto_increment,
    `e-mail` varchar(255) not null,
    `username` varchar(50) not null,
-   `password` text not null,
-   `logins` int(10) unsigned not null default '0',
-   `last_login` int(10) unsigned,
+   `password` char(75),
+   `autologin` char(40),
    PRIMARY KEY (`id`),
    UNIQUE KEY (`username`),
-   UNIQUE KEY (`e-mail`)
+   UNIQUE KEY (`e-mail`),
+   UNIQUE KEY (`autologin`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- | User invites (for registrations)
+CREATE TABLE `invites` (
+   `token` char(40) not null,
+   `e-mail` varchar(255) not null,
+   `inviter` int(11) unsigned not null,
+   `invitee` int(11) unsigned,
+   PRIMARY KEY (`token`),
+   UNIQUE KEY (`e-mail`),
+   KEY `inviter` (`inviter`),
+   KEY `invitee` (`invitee`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `invites` (`token`, `e-mail`, `inviter`, `invitee`) VALUES 
+('1ba8a4f1778c52e8d6e766acfce17cc87203ebd8', 'kim@burgestrand.se', '1', NULL);
+
+-- InnoDB thingies
 ALTER TABLE `roles_users`
   ADD CONSTRAINT `roles_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `roles_users_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `user_tokens`
-  ADD CONSTRAINT `user_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 ALTER TABLE `forums_roles`
   ADD CONSTRAINT `forums_roles_ibfk_1` FOREIGN KEY (`forum_id`) REFERENCES `forums` (`id`) ON DELETE CASCADE,

commit 134a702bfbce9e3d79c40ee893d623d107a2ac95

Fixes bug introduced by 807cd96ee7

application/classes/model/user.php
@@ -19,7 +19,10 @@
             );
             
             // Fix fields
-            $this->_fields['username'] = new Sprig_Field_Username;
+            $this->_fields['username'] = new Sprig_Field_Username(array(
+                'empty' => FALSE,
+                'unique' => TRUE,
+            ));
             
             $email = $this->_fields['email'];
             $email->column = 'e-mail';

application/classes/sprig/field/username.php
@@ -10,8 +10,6 @@
     {
         public $min_length = 2;
         public $max_length = 50;
-        public $empty = FALSE;
-        public $unique = TRUE;
     }
     
 /* End of file username.php */