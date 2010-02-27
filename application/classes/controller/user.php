Burgestrand/Anglarna-Stockholm

commit b867607aa86eb3f2d9b0a89dae583d141703563e

Add ability for user to change email/password

application/classes/controller/user.php
@@ -14,7 +14,29 @@
             }
             else
             {
-                $user = $this->auth->get_user();
+                $user = $this->auth->get_user()->load();
+                
+                if ($_POST)
+                {
+                    $values = arr::extract($_POST, array('email', 'password'), NULL);
+                    $values = array_filter($values, create_function('$x', 'return ! empty($x);'));
+                    
+                    try
+                    {
+                        $user = $user->values($values)->update();
+                        $this->message_add('Din användare har uppdaterats.');
+                    }
+                    catch (Validate_Exception $e)
+                    {
+                        foreach ($e->array->errors('user/index') as $message)
+                        {
+                            $this->message_add($message, 'error');
+                        }
+                    }
+                    
+                    $this->request->reload();
+                }
+                
                 $this->template->title = 'Information om ' . html::chars($user->username);
                 $this->template->content = View::factory('user/index')->set('user', $user);
             }

commit 4d1b4b06d534cc7f8dcfca9ff30b6bd197df8159

Fix two bugs introduced with b867607aa

application/classes/controller/user.php
@@ -23,7 +23,7 @@
                     
                     try
                     {
-                        $user = $user->values($values)->update();
+                        $user = $user->values($user->check($values))->update();
                         $this->message_add('Din användare har uppdaterats.');
                     }
                     catch (Validate_Exception $e)
@@ -50,7 +50,7 @@
             if ( ! empty($_POST))
             {
                 $email = arr::get($_POST, 'e-mail', '');
-                $user = Sprig::factory('user', array('email', $email))->load();
+                $user = Sprig::factory('user', array('email' => $email))->load();
                 
                 if ($user->loaded())
                 {