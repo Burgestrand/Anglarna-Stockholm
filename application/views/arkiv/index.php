088bc326870983a17138ff23ca3ce90e699b342b
Kim Burgestrand <kim@burgestrand.se>
2010-02-28 04:17:08 +0100

    Add content under arkiv/index

---
+++ application/views/arkiv/index.php
@@ -0,0 +1,43 @@
+<div class="box">
+    <h1>Arkiv</h1>
+    <p>
+        Välkommen till det i nuläget minimala Arkiv som vi erbjuder.
+        Tanken är den, att vi ska samla allt från texter till
+        animeringar som har anknytning till Blåvitt, fotboll och även
+        sport i allmänhet.
+    </p>
+    <p>
+        Om du skulle finna texter om oss på andra icke Blåvita sidor som
+        du ej ser på denna site, maila länken till oss. Vi välkomnar
+        materiel av alla de slag, så skicka dina alster till <?php echo
+        html::mailto('stockholm@anglarna.se') ?>!
+    </p>
+</div>
+<div class="box">
+    <h2>Länkarkiv</h2>
+    <dl>
+        <dt>Blåvitt</dt>
+            <dd>
+                <ul>
+                    <li><a href="http://www.ifkgoteborg.se/">IFK Göteborg</a></li>
+                    <li><a href="http://www.anglarna.se/">Supporterklubben Änglarna</a></li>
+                    <li><a href="http://www.svenskafans.com/fotboll/ifkgoteborg/">Svenska Fans — Alltid Blåvitt</a></li>
+                    <li><a href="http://www.baraben.com">BaraBen</a></li>
+                    <li><a href="http://web.comhem.se/~u96800871/index.htm">Kombarris GBG</a></li>
+                    <li><a href="http://hem.passagen.se/mala6694/">Änglarna Hofors</a></li>
+                    <li><a href="http://hem.bredband.net/b106302/valkommen.htm">Änglarna Trestad</a></li>
+                    <li><a href="http://skane.anglarna.se">Änglarna Skåne</a></li>
+                    <li><a href="http://www.anglarnanord.tk/">Änglarna Nord</a></li>
+                    <li><a href="http://hem.passagen.se/bolton/elig.html">ELIG</a></li>
+                    <li><a href="http://www.angelz.net">BrownBombers</a></li>
+                </ul>
+            </dd>
+    </dl>
+</div>
+<div class="box">
+    <h2>Textarkiv</h2>
+    <p>
+        Textarkivet är tomt! De gamla artiklarna är på gång; under tiden
+        får ni gärna tipsa oss om artiklar via <?php echo html::mailto('stockholm@anglarna.se') ?>!
+    </p>
+</div>
