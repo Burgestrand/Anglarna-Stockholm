d98dab10a7e915edc552a26e50ede4c085931715
Kim Burgestrand <kim@burgestrand.se>
2010-03-05 19:15:18 +0100

    Add translation to forum error messages

---
+++ application/messages/forum/post.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+    
+    return array(
+        'author' => array(
+            'not_empty' => 'Du måste ange ett nick att posta under.',
+        ),
+        'message' => array(
+            'not_empty' => 'Meddelandefältet får inte vara tomt.',
+        ),
+    );
+
+/* End of file post.php */
+/* Location: ./application/messages/forum/post.php */ 
