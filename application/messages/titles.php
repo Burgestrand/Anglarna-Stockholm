3b52cd2c9951b5af7e029603245c38ecd2e8d867
Kim Burgestrand <kim@burgestrand.se>
2010-03-06 22:51:52 +0100

    Add titles to om/ pages

--- application/messages/titles.php
+++ application/messages/titles.php
@@ -6,6 +6,8 @@
         'arkiv/index'   => 'Änglarna Stockholms arkiv',
         'galleri/index' => 'Änglarna Stockholms galleri',
         'kontakt/index' => 'Kontakta Änglarna Stockholm',
+        'om/index'      => 'Om Änglarna Stockholm',
+        'om/hemsidan'   => 'Information om Änglarna Stockholms hemsida',
         'forum/'        => 'Änglarna Stockholms forum',
     );
     
