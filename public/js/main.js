890f8cce82980ac10884b096246091f4dc52f208
Kim Burgestrand <kim@burgestrand.se>
2010-03-13 20:19:21 +0100

    Update shadowbox to include images

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,18 @@
-// Find all youtube-links
-$('a[href*=youtube.com]').each(function () {
-    //var href = $(this).attr('href').replace(/\?|&|=/g, function (v) { return escape(v) })
-    var href = $(this).attr('href').replace(/watch\?v=([^&]+)/, 'v/$1')
+// Go through all links
+$('a[href]').each(function () {
+    var href = $(this).attr('href')
     
-    $(this).attr('rel', 'shadowbox[youtube];height=385;width=480;player=swf;content=' + href)
+    if (href.match(/youtube\.com\/(v|watch)/i))
+    {
+        //var href = $(this).attr('href').replace(/\?|&|=/g, function (v) { return escape(v) })
+        var href = href.replace(/watch\?v=([^&]+)/, 'v/$1')
+        
+        $(this).attr('rel', 'shadowbox[youtube];height=385;width=480;player=swf;content=' + href)
+    }
+    else if (href.match(/\.(jpe?g|png|gif|mov|swf)/))
+    {
+        $(this).attr('rel', 'shadowbox')
+    }
 })
 
 Shadowbox.init({
